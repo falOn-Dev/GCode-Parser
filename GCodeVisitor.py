10af4596d6441b58164034483db1a3faf07b6b3a
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-03-01 20:39:00 -0500

    Update GCodeVisitor.py

--- GCodeVisitor.py
+++ GCodeVisitor.py
@@ -1,21 +1,34 @@
 from generated_code.gcode_grammarParser import gcode_grammarParser
 from generated_code.gcode_grammarVisitor import gcode_grammarVisitor
+import processing as proc
 
 
 class GCodeVisitor(gcode_grammarVisitor):
     def visitProgram(self, ctx: gcode_grammarParser.ProgramContext):
-        for command_ctx in ctx.command():
-            self.visitCommand(command_ctx)
+        for command in ctx.command():
+            self.visitCommand(command)
 
     def visitCommand(self, ctx: gcode_grammarParser.CommandContext):
         if ctx.simple_motion_command():
             self.visitSimple_motion_command(ctx.simple_motion_command())
 
     def visitSimple_motion_command(self, ctx: gcode_grammarParser.Simple_motion_commandContext):
-        motion_type = ctx.getChild(0).getText()
-        x_arg = ctx.X_ARG().getText() if ctx.X_ARG() else None
-        y_arg = ctx.Y_ARG().getText() if ctx.Y_ARG() else None
-        z_arg = ctx.Z_ARG().getText() if ctx.Z_ARG() else None
-        e_arg = ctx.EXTRUSION_ARG().getText() if ctx.EXTRUSION_ARG() else None
-        f_arg = ctx.FEEDRATE_ARG().getText() if ctx.FEEDRATE_ARG() else None
-        print(f"Motion type: {motion_type}, X: {x_arg}, Y: {y_arg}, Z: {z_arg}, E: {e_arg}, F: {f_arg}")
+        args = []
+        for arg in ctx.arguement():
+            args.append(self.visitArguement(arg))
+        motion_type = ctx.getChild(0).getText() if ctx.getChild(0) else None
+
+        if motion_type == "G0":
+            print("Rapid move with args:")
+            for arg in args:
+                proc.handle_arguement(arg[0], arg[1])
+        elif motion_type == "G1":
+            print("Linear move with args:")
+            for arg in args:
+                proc.handle_arguement(arg[0], arg[1])
+        print("\n")
+
+    def visitArguement(self, ctx: gcode_grammarParser.ArguementContext):
+        key = ctx.ID().getText()
+        number = float(ctx.NUMBER().getText())
+        return [key, number]
